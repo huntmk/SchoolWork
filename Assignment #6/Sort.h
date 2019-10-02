0142d84bf7cd4ad70d7e62ef5d45548f7373853d
huntmk <huntmk@iupui.edu>
2019-10-01 23:16:18 -0400

    CS_240 (Computing II) Assignment 6

---
+++ Assignment #6/Sort.h
@@ -0,0 +1,20 @@
+// 
+//Honor Pledge:
+//
+// I pledge that I have neither given nor 
+// received any help on this assignment
+//.
+//
+// huntmk
+#ifndef SORT_H
+#define SORT_H
+class Sort
+{
+	public: 
+		//pure virtual sort method
+		virtual void sort(int*, int)= 0;
+		//destructor
+		virtual ~Sort(){}
+};
+
+#endif//SORT_H
